771df9e84ab984c256498ad2219230e3f1dbb97c
likeky <likeky@163.com>
2018-01-24 00:52:41 +0800

    aa

--- wx_program/DogYear/pages/seller/sellerInfo.js
+++ wx_program/DogYear/pages/seller/sellerInfo.js
@@ -5,7 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    typeList: ["其他", "美食餐饮", "商超购物", "生活服务","休闲娱乐","鞋子箱包","服饰"],
+    index: 0
   },
 
   /**
@@ -16,51 +17,17 @@ Page({
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
   
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+  onShopType:function(e)
+  {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
