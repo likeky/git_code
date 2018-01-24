aac6d7989904bc50943d4e29600526bbf1419e8f
likeky <likeky@163.com>
2018-01-25 00:58:38 +0800

    update

--- wx_program/DogYear/pages/seller/sellerInfo.js
+++ wx_program/DogYear/pages/seller/sellerInfo.js
@@ -6,7 +6,8 @@ Page({
    */
   data: {
     typeList: ["其他", "美食餐饮", "商超购物", "生活服务","休闲娱乐","鞋子箱包","服饰"],
-    index: 0
+    index: 0,
+    logoSrc:"/resource/arrow.png"
   },
 
   /**
@@ -30,4 +31,61 @@ Page({
       index: e.detail.value
     })
   },
+
+  onGetLogo:function(e)
+  {
+
+    /*
+    this.setData({
+      logoSrc: "sdfsdfsdfsdf"
+    })
+    
+
+    var tmp2 = this.data.logoSrc;
+    console.log("tmp2=>" + tmp2);
+    */
+
+   
+    //var tmpThis=this;
+    wx.chooseImage
+    ({
+        count: 1, // 默认9    
+        sizeType: ['compressed'], // 可以指定是原图还是压缩图，默认二者都有
+        sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+
+        success: (res) => {
+          var tempFilePaths = res.tempFilePaths;
+          var imgSrc = tempFilePaths[0];
+          //console.log("imgSrc=>" + imgSrc);
+
+          this.setData({
+            logoSrc: imgSrc
+          })
+
+          //var tmp2 = this.data.logoSrc;
+          //console.log("tmp2=>" + tmp2);
+        }
+
+        /*
+        success: function (res) 
+        {
+          // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+          var tempFilePaths = res.tempFilePaths;
+          var imgSrc=tempFilePaths[0];
+          //console.log("imgSrc=>" + imgSrc);
+
+          tmpThis.setData({
+            logoSrc: imgSrc
+          })
+
+          var tmp2 = tmpThis.data.logoSrc;
+          console.log("tmp2=>" + tmp2);
+          
+        }
+        */
+        
+    });
+      
+
+  }
 })
